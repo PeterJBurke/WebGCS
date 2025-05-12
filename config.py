0d42c4413127f9d8f0ca55e5c43f8fcd4e8b2a25
PeterJBurke <peter.burke.john@gmail.com>
2025-05-11 22:38:02 -0700

    Fix: Add missing variables to config.py for compatibility

--- config.py
+++ config.py
@@ -9,17 +9,22 @@ load_dotenv()
 DRONE_TCP_ADDRESS = os.getenv('DRONE_TCP_ADDRESS', '192.168.193.46')  # Drone IP address
 DRONE_TCP_PORT = os.getenv('DRONE_TCP_PORT', '5678')  # Drone port
 MAVLINK_CONNECTION_STRING = f'tcp:{DRONE_TCP_ADDRESS}:{DRONE_TCP_PORT}'
+DRONE_BAUD_RATE = int(os.getenv('DRONE_BAUD_RATE', 115200)) # Added Drone Baud Rate
+MAVLINK_SOURCE_SYSTEM = int(os.getenv('MAVLINK_SOURCE_SYSTEM', 255)) # Added MAVLink Source System
 
 # Web Server Settings
 WEB_SERVER_HOST = os.getenv('WEB_SERVER_HOST', 'localhost')
 WEB_SERVER_PORT = int(os.getenv('WEB_SERVER_PORT', '5000'))
 SECRET_KEY = os.getenv('SECRET_KEY', 'desktop_drone_secret!')
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true' # Added DEBUG setting
 
 # MAVLink Settings
 HEARTBEAT_TIMEOUT = int(os.getenv('HEARTBEAT_TIMEOUT', '30'))
 REQUEST_STREAM_RATE_HZ = int(os.getenv('REQUEST_STREAM_RATE_HZ', '4'))
 COMMAND_ACK_TIMEOUT = int(os.getenv('COMMAND_ACK_TIMEOUT', '10'))
 TELEMETRY_UPDATE_INTERVAL = float(os.getenv('TELEMETRY_UPDATE_INTERVAL', '0.1'))  # Seconds (10 Hz)
+MAVLINK_DIALECT = os.getenv('MAVLINK_DIALECT', 'ardupilotmega') # Added MAVLink dialect
+RECONNECTION_ATTEMPT_DELAY = os.getenv('RECONNECTION_ATTEMPT_DELAY', 5) # Added reconnection delay
 
 # ArduPilot Custom Flight Modes
 AP_CUSTOM_MODES = {
